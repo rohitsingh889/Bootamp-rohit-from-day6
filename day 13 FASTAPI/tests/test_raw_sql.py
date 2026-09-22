78ac3502764ed084885e2aa9876ff767f630d9f3
Rohit Singh <rohit517929@gmail.com>
2026-09-22 11:49:17 +0530

    commit

---
+++ day 13 FASTAPI/tests/test_raw_sql.py
@@ -0,0 +1,35 @@
+from sqlalchemy import text
+
+from app.db.session import SessionLocal
+
+
+def test_raw_sql():
+
+    db = SessionLocal()
+
+    try:
+        query = text("""
+            SELECT
+                id,
+                name,
+                email,
+                department,
+                designation,
+                salary,
+                is_active
+            FROM employees
+        """)
+
+        result = db.execute(query)
+
+        employees = result.mappings().all()
+
+        for employee in employees:
+            print(employee)
+
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    test_raw_sql()
